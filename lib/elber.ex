d52214a355c4dc6b239ceefd6ee3cf1a80c9393d
Jim Pringle <pringlized@gmail.com>
2017-05-24 18:12:45 -0700

    Let's do this

---
+++ lib/elber.ex
@@ -0,0 +1,18 @@
+defmodule Elber do
+  @moduledoc """
+  Documentation for Elber.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elber.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elber_test.exs
@@ -0,0 +1,8 @@
+defmodule ElberTest do
+  use ExUnit.Case
+  doctest Elber
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
